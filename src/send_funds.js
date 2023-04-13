9ad91cbfe7754d7b23f606ccefae9f41c27d98ce
Boris L <boris@agora-labs.com>
2023-04-13 15:03:15 -0700

    Fixes

--- src/send_funds.js
+++ src/send_funds.js
@@ -1,6 +1,7 @@
 // send_funds.js
 require('dotenv').config();
 var Web3 = require('web3');
+var fs = require('fs');
 var web3 = new Web3(new Web3.providers.IpcProvider('/home/ec2-user/geth_data/geth.ipc', require('net')));
 
 var from = process.env.BANK_ADDRESS;
@@ -8,7 +9,7 @@ var to = process.argv[2];
 var amount = process.argv[3];
 
 // Assuming you have the keystore file
-var keystore = require(process.env.KEYSTORE_PATH);
+var keystore = fs.readFileSync(process.env.KEYSTORE_PATH);
 var password = process.env.KEYSTORE_PWD;
 
 web3.eth.accounts.wallet.decrypt(keystore, password);
